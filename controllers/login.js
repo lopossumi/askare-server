baf94f26ed25acd2c55da001d4b2b1417e9f2b64
Mikko Loponen <mikko.loponen@iki.fi>
2018-05-31 23:05:44 +0300

    capitalization2

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
-const User = require('../models/user')
+const User = require('../models/User')
 
 loginRouter.post('/', async (request, response) => {
     const body = request.body
