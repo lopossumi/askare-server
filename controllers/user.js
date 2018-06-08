1883a75ba2d93204b32b0459209ed4d97698714a
Mikko Loponen <mikko.loponen@iki.fi>
2018-06-08 11:51:35 +0300

    new user account validation

--- controllers/user.js
+++ controllers/user.js
@@ -5,15 +5,27 @@ const User = require('../models/User')
 userRouter.post('/', async (request, response) => {
     try {
         const body = request.body
+        const usernameRegex = /^[a-zA-Z0-9]+$/
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if (!body.username.match(usernameRegex)) {
+            return response.status(400).send({ error: 'Invalid username. Please use only alphanumeric characters.' })
+        }
+
+        if (!body.email.match(emailRegex)) {
+            return response.status(400).send({ error: 'Invalid email. Please enter a valid email address.' })
+        }
 
         if (body.password.length < 6) {
-            return response.status(400).send({ error: 'Insufficient password length' })
+            return response.status(400).send({ error: 'Invalid password. Use atleast 6 characters.' })
         }
+
         if (await User.findOne({ username: body.username })) {
-            return response.status(400).send({ error: 'Username already taken' })
+            return response.status(400).send({ error: 'Invalid username. This username is already taken.' })
         }
+
         if (await User.findOne({ email: body.email })) {
-            return response.status(400).send({ error: 'E-mail already taken' })
+            return response.status(400).send({ error: 'Invalid email. This email address is already taken.' })
         }
 
         const saltRounds = 10
